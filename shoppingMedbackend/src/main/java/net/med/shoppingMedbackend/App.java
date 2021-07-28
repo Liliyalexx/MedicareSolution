b73f5dfdd2e1a2dd097217b18f3e7930e18d145f
Liliyalexx <liliya.fedyurina@gmail.com>
2021-07-27 18:21:06 -0700

    My Project Setup Completed Successfully

---
+++ shoppingMedbackend/src/main/java/net/med/shoppingMedbackend/App.java
@@ -0,0 +1,13 @@
+package net.med.shoppingMedbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
